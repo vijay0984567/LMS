7bab47a0bb0f0c167bc082332499dbbe56f09f23
Vijay Kumar <159708609+vijay0984567@users.noreply.github.com>
2025-04-11 21:09:47 +0530

    Update courseApi.js

--- client/src/features/api/courseApi.js
+++ client/src/features/api/courseApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_API = "http://localhost:8080/api/v1/course";
+const COURSE_API = "https://lms-server-26gi.onrender.com/api/v1/course";
 
 export const courseApi = createApi({
   reducerPath: "courseApi",
