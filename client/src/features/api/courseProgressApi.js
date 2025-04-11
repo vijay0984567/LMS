c59a144d7460f27b9ce5c7ade34ea3957a5da38e
Vijay Kumar <159708609+vijay0984567@users.noreply.github.com>
2025-04-11 21:10:26 +0530

    Update courseProgressApi.js

--- client/src/features/api/courseProgressApi.js
+++ client/src/features/api/courseProgressApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_PROGRESS_API = "http://localhost:8080/api/v1/progress";
+const COURSE_PROGRESS_API = "https://lms-server-26gi.onrender.com/api/v1/progress";
 
 export const courseProgressApi = createApi({
   reducerPath: "courseProgressApi",
